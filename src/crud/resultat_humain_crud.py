323f70647b00dfe4459e6e4fdde82e17be4761ee
Kaouter <kaouter1.rhazlani@gmail.com>
2026-01-13 21:56:17 +0100

    feat: creation des scripts de crud pour chaque tables

---
+++ src/crud/resultat_humain_crud.py
@@ -0,0 +1,20 @@
+from src.database.load_database import get_connection
+
+def insert_resultat_humain(data: dict):
+    query = """
+    INSERT INTO cross_sec.resultats_humain (
+        operation_id, categorie_personne,
+        resultat_humain, nombre, dont_nombre_blesse
+    )
+    VALUES (
+        %(operation_id)s, %(categorie_personne)s,
+        %(resultat_humain)s, %(nombre)s,
+        %(dont_nombre_blesse)s
+    );
+    """
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute(query, data)
+    conn.commit()
+    cur.close()
+    conn.close()
