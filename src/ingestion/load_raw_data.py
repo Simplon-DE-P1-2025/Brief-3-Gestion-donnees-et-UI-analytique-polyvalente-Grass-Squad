1765ffc5a89fb8047c152be7d562c43f99e94e0d
Kaouter <kaouter1.rhazlani@gmail.com>
2026-01-13 22:13:02 +0100

    refactor: organisation du code d'ingestion

--- src/ingestion/load_raw_data.py
+++ src/ingestion/load_raw_data.py
@@ -11,9 +11,12 @@ from config import RAW_DATA_DIR
 from io_pipeline import load_data
 
 
-def load_all_raw_data() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def load_all_raw_data() -> dict:
     """
-    Charge les 4 sources de données brutes et retourne 4 DataFrames
+    Charge les 4 sources de données brutes et retourne un dictionnaire de DataFrames
+    
+    Returns:
+        dict: Dictionnaire avec les clés 'operations', 'operations_stats', 'flotteurs', 'resultats_humain'
     """
 
     # Définition des paths des sources
@@ -28,33 +31,10 @@ def load_all_raw_data() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.Da
     df_flotteurs = load_data("csv", flotteurs_path)
     df_resultats_humain = load_data("csv", resultats_humain_path)
 
-    return (
-        df_operations,
-        df_operations_stats,
-        df_flotteurs,
-        df_resultats_humain
-    )
-
-
-if __name__ == "__main__":
-    (
-        df_operations,
-        df_operations_stats,
-        df_flotteurs,
-        df_resultats_humain
-    ) = load_all_raw_data()
-
-    print("Aperçu des données chargées :")
-    
-    print("\ndf_operations shape : :", df_operations.shape)
-    print("df_operations :", df_operations.head(5))
-    
-    print("\ndf_operations_stats shape : :", df_operations_stats.shape)
-    print("df_operations_stats :", df_operations_stats.head(5))
-    
-    print("\ndf_flotteurs shape : :", df_flotteurs.shape)
-    print("df_flotteurs :", df_flotteurs.head(5))
-    
-    print("\ndf_resultats_humain shape : :", df_resultats_humain.shape)
-    print("df_resultats_humain :", df_resultats_humain.head(5))
+    return {
+        'operations': df_operations,
+        'operations_stats': df_operations_stats,
+        'flotteurs': df_flotteurs,
+        'resultats_humain': df_resultats_humain
+    }
 
