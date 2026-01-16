67b78e998dc2d7b9f27dec444894f7878f985387
Kaouter <kaouter1.rhazlani@gmail.com>
2026-01-16 09:29:53 +0100

    feat : streamlit app mise ajour dashboard

--- src/app/views/dashboard_flotteurs.py
+++ src/app/views/dashboard_flotteurs.py
@@ -38,10 +38,6 @@ def render(engine):
     params["min_ops"] = min_ops
 
 
-    if st.button("⬅️ Retour au dashboard", key="btn_back_to_dashboard_flotteurs"):
-        st.session_state.action = 'dashboard'
-        st.rerun()
-    # --------------
 
     
     # ----------------------------------
